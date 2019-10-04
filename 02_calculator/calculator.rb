88c4baee77d23c383c1d77ed45e09d11a9c4125a
Mallory Robertson <malloryworks@gmail.com>
2019-10-03 18:58:22 -0700

    02 Calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,23 @@
-#write your code here
+def add(num1, num2)
+  num1 + num2
+end
+
+def subtract(num1, num2)
+  num1 - num2
+end
+
+def sum(array)
+  sum = 0
+  if (array.length !=0)
+    array.each do |i|
+      sum += i
+    end
+    sum * 1
+  else
+    sum
+  end
+end
+
+def multiply(*args)
+  args.reduce(:*)
+end
--- 02_calculator/calculator_spec.rb
+++ 02_calculator/calculator_spec.rb
@@ -79,9 +79,13 @@ end
 
 describe "#multiply" do
 
-  it "multiplies two numbers"
+  it "multiplies two numbers" do
+    expect(multiply(2,3)).to eq(6)
+  end
 
-  it "multiplies several numbers"
+  it "multiplies several numbers" do
+    expect(multiply(2,3,5)).to eq(30)
+  end
   
 end
 
