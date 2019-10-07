5ffd4362a5a052b83bf90778d9cae2deb7368031
Mallory Robertson <malloryworks@gmail.com>
2019-10-07 15:25:39 -0700

    Finished 05_book title

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,15 @@
 class Book
-# write your code here
+    attr_accessor :title # gives read and write access to title symbol
+
+    def title=(book_title)
+        title_arr = book_title.split(" ") # split words into an array
+        title_arr = [title_arr[0].capitalize] + title_arr[1..-1].map do |i| # capitalize the first letter
+            if ["and", "in", "the", "of", "a", "an"].include?(i)
+                i # don't capitalize any of these words
+            else
+                i.capitalize
+            end
+        end
+        @title = title_arr.join(" ")
+    end
 end
