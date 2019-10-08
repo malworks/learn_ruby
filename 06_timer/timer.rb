479b5107df4f6766bda8eea32da7e7bd230b7414
Mallory Robertson <malloryworks@gmail.com>
2019-10-07 19:30:54 -0700

    Started timer exercise

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,23 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds, :time_string
+
+  def initialize
+    @seconds = 0
+  end
+
+  def placement(i)
+    if i < 10
+      "0#{i}"
+    else
+      "#{i}"
+    end
+  end
+
+  def time_string
+    if seconds == 0
+      "00:00:00"
+    else
+    "00:00:#{seconds}"
+    end
+  end
 end
