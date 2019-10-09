d67224f6e2f37a3c364e242a13281f6d0dd02d8d
Mallory Robertson <malloryworks@gmail.com>
2019-10-09 16:09:40 -0700

    Finished timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -3,6 +3,8 @@ class Timer
 
   def initialize
     @seconds = 0
+    @minutes = 0
+    @hours = 0
   end
 
   def placement(i)
@@ -14,10 +16,37 @@ class Timer
   end
 
   def time_string
-    if seconds == 0
-      "00:00:00"
+    @minutes = @seconds / 60
+    @seconds = @seconds % 60
+    @hours = @minutes / 60
+    @minutes = @minutes % 60
+    "#{placement(@hours)}:#{placement(@minutes)}:#{placement(@seconds)}"
+  end
+
+  def placement(num)
+    if num == 0
+      "00"
+    elsif num > 0 and num < 10
+      "0#{num}"
     else
-    "00:00:#{seconds}"
+      "#{num}"
     end
   end
 end
+
+
+# Official solution
+# class Timer
+#   attr_accessor :seconds
+
+#   def initialize
+#     @seconds = 0
+#   end
+
+#   def time_string
+#     hours = @seconds/3600
+#     remainder = @seconds%3600
+#     sprintf("%02d:%02d:%02d", hours, remainder/60, remainder%60)
+#   end
+  
+# end
